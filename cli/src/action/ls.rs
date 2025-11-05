aee832ac3c63346f8c7e8e0a048809e50691e9e2
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-11-05 20:31:39 -0300

    refactor(cli): ditch unnecessary generic

--- cli/src/action/ls.rs
+++ cli/src/action/ls.rs
@@ -1,7 +1,8 @@
 use quartz_core::{
-    Quartz, QuartzResult, ctx::Ctx, editor::Editor, endpoint::EndpointHandle, state::StateField,
-    tree::Node,
+    Quartz, QuartzResult, ctx::Ctx, endpoint::EndpointHandle, state::StateField, tree::Node,
 };
+
+use crate::editor::FileEditor;
 
 #[derive(clap::Args, Debug)]
 pub struct Args {
@@ -13,7 +14,7 @@ pub struct Args {
     pub depth: Option<usize>,
 }
 
-pub fn cmd<E: Editor>(args: Args, quartz: Quartz<E>) -> QuartzResult {
+pub fn cmd(args: Args, quartz: Quartz<FileEditor>) -> QuartzResult {
     let tree_root = quartz.handle_tree(args.handle);
     let current_handle = StateField::Endpoint.get(&quartz.ctx).ok();
     output_tree(&quartz.ctx, tree_root.root, current_handle, 0);
