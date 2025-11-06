342f89197436d3b14b8a87c569ca33a47e3cc75a
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-11-06 20:10:59 -0300

    refactor(editor): fetch current editor as a core logic

--- cli/src/editor/mod.rs
+++ cli/src/editor/mod.rs
@@ -5,7 +5,7 @@ pub struct FileEditor {}
 
 impl Editor for FileEditor {
     fn edit<E: Editor>(&self, quartz: &Quartz<E>, file_path: &std::path::PathBuf) -> QuartzResult {
-        let editor = quartz.config_get("preferences.editor")?;
+        let editor = self.editor(quartz)?;
         let _ = std::process::Command::new(editor)
             .arg(file_path)
             .status()
--- core/src/editor/mod.rs
+++ core/src/editor/mod.rs
@@ -4,4 +4,9 @@ use crate::{Quartz, QuartzResult};
 
 pub trait Editor {
     fn edit<E: Editor>(&self, quartz: &Quartz<E>, file_path: &PathBuf) -> QuartzResult;
+    fn editor<E: Editor>(&self, quartz: &Quartz<E>) -> QuartzResult<String> {
+        let current_editor = quartz.config_get("preferences.editor")?;
+
+        Ok(current_editor)
+    }
 }
