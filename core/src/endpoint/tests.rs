4e57906f1b4b02961d7d1301dea0fb5ace8127c0
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-11-22 11:55:44 -0300

    chore(endpoint): remove some unnecessary integration tests

--- core/src/endpoint/tests.rs
+++ core/src/endpoint/tests.rs
@@ -24,3 +24,18 @@ fn test_recursive_cp_handle() {
     assert!(EndpointHandle::from("testing-copy/ex2").exists(&quartz.inner));
     assert!(EndpointHandle::from("testing-copy/ex2/sub").exists(&quartz.inner));
 }
+
+#[test]
+fn test_non_recursive_cp_handle() {
+    let quartz = TestQuartz::empty().unwrap();
+    quartz.handle_create("testing").unwrap();
+    quartz.handle_create("testing/ex").unwrap();
+    quartz.handle_create("testing/ex2").unwrap();
+    quartz.handle_create("testing/ex2/sub").unwrap();
+    quartz.handle_cp(false, "testing", "testing-copy").unwrap();
+
+    assert!(EndpointHandle::from("testing-copy").exists(&quartz.inner));
+    assert!(!EndpointHandle::from("testing-copy/ex").exists(&quartz.inner));
+    assert!(!EndpointHandle::from("testing-copy/ex2").exists(&quartz.inner));
+    assert!(!EndpointHandle::from("testing-copy/ex2/sub").exists(&quartz.inner));
+}
