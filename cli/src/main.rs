20e6ec49ec8af209bda84a837d11d1a013e3dd28
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-11-29 17:18:02 -0300

    fix(cli): lack of pretty error printing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,7 +6,6 @@ mod validator;
 use std::env::current_dir;
 
 use clap::Parser;
-use colored::Colorize;
 
 use crate::{
     cli::{Cli, Cmd},
@@ -17,20 +16,7 @@ use quartz_core::{
     error::{QuartzError, QuartzResult},
 };
 
-#[tokio::main]
-async fn main() -> QuartzResult {
-    std::panic::set_hook(Box::new(|info| {
-        let payload = if let Some(s) = info.payload().downcast_ref::<String>() {
-            s.clone()
-        } else if let Some(s) = info.payload().downcast_ref::<&str>() {
-            s.to_string()
-        } else {
-            info.to_string()
-        };
-
-        eprintln!("{}: {payload}", "error".red().bold());
-    }));
-
+async fn entrypoint() -> QuartzResult {
     let args = Cli::parse();
 
     // Has to run outside action flow because it cannot resolve `ctx`.
@@ -46,13 +32,22 @@ async fn main() -> QuartzResult {
     if let Some(handle) = args.from_handle {
         quartz.handle_switch(handle)?;
     }
+
+    // When true, ensures pagers and/or grep keeps the output colored
     colored::control::set_override(quartz.config().parse().ui.colors());
 
     let ctx = Ctx::new(quartz);
 
-    // When true, ensures pagers and/or grep keeps the output colored
-
     action::cmd(ctx, args.command).await?;
 
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> QuartzResult {
+    if let Err(error) = entrypoint().await {
+        eprintln!("{}", error);
+        std::process::exit(1);
+    }
     Ok(())
 }
